e1091378a0090611b714b2b74a42c8bdd82a3918
kess13 <bohhhdan13@gmail.com>
2023-10-12 22:03:13 +0200

    fisrt error because max_element returns iterator

--- radix.cc
+++ radix.cc
@@ -1,18 +1,35 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-vector <int> countings(vector<int> vec)
+#include <algorithm>
+vector <int> countingsort(vector<int> vec)
 {
-vector <int> copy;
+vector <int> copy=vec;
+auto max = max_element(vec.begin(), vec.end());
+string biggest = to_string(max);
+for (int i=biggest.length()-1; i>0; i--)
+{
+for (int i=0; i<vec.size(); i++)
+{
+int a=vec[i]%10;
+if (a==0)
 
+}
+
+
+}
 }
 
 
 
 int main()
 {
-vector <int> vec = {0,11,32,43,554,15,6,337}
+vector <int> vec = {0,11,32,43,554,15,6,337};
+vec=countingsort(vec);
+for (int i : vec)
+{
+cout << " " <<i;
 
+}
 
 }
