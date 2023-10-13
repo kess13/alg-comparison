09418516bbb45fafe29b341c0f6a6ef981e4454e
kess13 <bohhhdan13@gmail.com>
2023-10-13 12:35:10 +0200

    massive changes, begin to create countint sort func for radix sort

--- radix.cc
+++ radix.cc
@@ -7,8 +7,26 @@ vector <int> countingsort(vector<int> vec)
 vector <int> copy=vec;
 auto max = max_element(vec.begin(), vec.end());
 int maxel= *max;
+int temp=1;
+vector<int> countarr(maxel+1);//vector wiht size max value +1 fill with zeroes
+for (int i=0; i<vec.size(); i++)
+{
+countarr[i]=count(countarr.begin(), countarr.end(), i);
 
-string biggest =to_string(maxel);
+
+
+}
+
+return vec;
+
+}
+
+
+
+
+int main()
+{
+    string biggest =to_string(maxel);
 for (int i=biggest.length(); i>0; i--)
 {
     vector <int> copy=vec;
@@ -20,16 +38,7 @@ copy.push_back(a);
 if ()
 
 }
-
-
 }
-return vec;
-}
-
-
-
-int main()
-{
 vector <int> vec = {0,11,32,43,554,15,6,337};
 vec=countingsort(vec);
 for (int i : vec)
