844fc39ef2bb6d73eace736a7f564419636831c1
kess13 <bohhhdan13@gmail.com>
2023-10-13 13:38:35 +0200

    fixed for loops conditions, trying to understand what to do with last step

--- radix.cc
+++ radix.cc
@@ -4,20 +4,26 @@ using namespace std;
 #include <algorithm>
 vector <int> countingsort(vector<int> vec)
 {
-vector <int> copy=vec;
+
 auto max = max_element(vec.begin(), vec.end());
 int maxel= *max;
-int temp=1;
-vector<int> countarr(maxel+1);//vector wiht size max value +1 fill with zeroes
-for (int i=0; i<vec.size(); i++)
+int temp=0;
+vector<int> countarr(maxel+1,0);//vector wiht size max value +1 filled with zeroes
+for (int i=0; i <= maxel; i++)
 {
-countarr[i]=count(countarr.begin(), countarr.end(), i);
-
-
-
+countarr[i]=count(vec.begin(), vec.end(), i);
 }
-
-return vec;
+for (int i=0; i <= maxel; i++) //to make cumulative sum of count array elements
+{
+temp=temp+countarr[i];
+countarr[i]=temp;
+}
+ for (int i = 0; i <= vec.size()-1; i++) {
+    
+        vec[countarr[vec[i]] - 1] = vec[i];
+        
+    }
+    return vec;
 
 }
 
@@ -26,20 +32,8 @@ return vec;
 
 int main()
 {
-    string biggest =to_string(maxel);
-for (int i=biggest.length(); i>0; i--)
-{
-    vector <int> copy=vec;
-for (int i=0; i<vec.size(); i++)
-{
-    
-int a=vec[i]%10;
-copy.push_back(a);
-if ()
 
-}
-}
-vector <int> vec = {0,11,32,43,554,15,6,337};
+vector <int> vec = {0,1,9,3,5,2,4};
 vec=countingsort(vec);
 for (int i : vec)
 {
