8cce8ded6ff66476898c4e223db319c9e06843f8
kess13 <bohhhdan13@gmail.com>
2023-10-14 14:14:00 +0200

    started to implement radix sort inside count sort

--- radix.cc
+++ radix.cc
@@ -4,16 +4,28 @@ using namespace std;
 #include <algorithm>
 vector <int> countingsort(vector<int> vec)
 {
-
+    vector <int> MainCopy=vec;
+    vector <int> SecondCopy=vec;
 auto max = max_element(vec.begin(), vec.end());
 int maxel= *max;
+string maxstr= to_string(maxel);
+for (int j=maxstr.length(); j>0;j--){
+    for (int i=0; i < vec.size(); i++)
+{
+MainCopy[i]=SecondCopy[i]%10;// to extract last digit 
+SecondCopy[i]=SecondCopy[i]/10;// to erase last digit
+
+
+}
+}
+
 int temp=0;
-vector<int> countarr(maxel+1,0);//vector wiht size max value +1 filled with zeroes
+vector<int> countarr(maxel+1,0);
 for (int i=0; i <= maxel; i++)
 {
 countarr[i]=count(vec.begin(), vec.end(), i);
 }
-for (int i=0; i <= maxel; i++) //to make cumulative sum of count array elements
+for (int i=0; i <= maxel; i++) 
 {
 temp=temp+countarr[i];
 countarr[i]=temp;
@@ -33,8 +45,8 @@ vector<int> res(vec.size());
 int main()
 {
 
-vector <int> vec = {0,1,9,3,5,2,4,0,6,8};
-vec=countingsort(vec);
+vector <int> vec = {0,13,129,3,5,442,42,0,61,28};
+
 for (int i : vec)
 {
 cout << " " <<i;
