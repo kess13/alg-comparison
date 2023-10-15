21065517eeddfb8c664f5505dc3f7a64b79549a0
kess13 <bohhhdan13@gmail.com>
2023-10-15 15:46:57 +0200

    first steps heap sort

---
+++ heap.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#include <algorithm>
+void MakeHeap(vector<int> vec)
+{
+int LeftChild = 0;//2*i+1
+int RightChild = 0;//2*i+2
+int Parent = 0;//floor i/2
+int MainEl=0;//i
+
+
+
+}
+
+
+
+int main()
+{
+
+vector <int> vec = {0,1,9,3,35,42,4,0,55,8};
+
+
+for (int i : vec)
+{
+cout << " " <<i;
+
+}
+
+}
