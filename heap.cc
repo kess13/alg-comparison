f3751f8aa187e8573900be3b0a5449e7a7a6a818
kess13 <bohhhdan13@gmail.com>
2023-10-17 14:50:15 +0200

    now i have a right sort where the largest element stand on the 0 index to make max heap property

--- heap.cc
+++ heap.cc
@@ -6,40 +6,37 @@ using namespace std;
 
 
 
-void heapify(vector<int> &vec, int i)
-{
-    int size = vec.size()-i;
-int CurrentEl=i;
-int LeftChild = 2*i+1; // we need to make both children  be smaller than parent
-int RightChild = 2*i+2;
-if (LeftChild<size  && RightChild<size){
-   while (max(vec[LeftChild],vec[RightChild])>vec[CurrentEl])
-   {
-        if (vec[LeftChild]>vec[RightChild])
-        {
-            if (vec[LeftChild]>vec[CurrentEl])
-            
-            swap(vec[LeftChild],vec[CurrentEl]);}
-        else 
-        {
-              if (vec[RightChild]>vec[CurrentEl])
-            swap(vec[CurrentEl],vec[RightChild]);
-            
-        } 
+void heapify(vector<int> &vec, int i, int size) {
+    int CurrentEl = i;
+    int LeftChild = 2 * i + 1;
+    int RightChild = 2 * i + 2;
+    if (LeftChild < size && RightChild < size) {
+        while (max(vec[LeftChild], vec[RightChild]) > vec[CurrentEl]) {
+            if (vec[LeftChild] > vec[RightChild]) {
+                if (vec[LeftChild] > vec[CurrentEl]) {
+                    swap(vec[LeftChild], vec[CurrentEl]);
+                }
+            } else {
+                if (vec[RightChild] > vec[CurrentEl]) {
+                    swap(vec[CurrentEl], vec[RightChild]);
+                }
+            }
+        }
+    }
+}
 
 
-   }
+
+void MakeHeap(vector<int> &vec, int heap_size) {
+    for (int i = heap_size / 2 - 1; i >= 0; i--) {
+        heapify(vec, i, heap_size);
+    }
+    
+    
+    swap(vec[0], vec[heap_size - 1]);
 
 }
 
-}
-void MakeHeap (int heap_size, vector<int> &vec)
-{
-for (int i = 0; i<heap_size; i++)
-{
-heapify(vec, i);
-}
-}
 
 
 
@@ -50,14 +47,15 @@ heapify(vec, i);
 
 int main()
 {
+    vector<int> vec = {0, 1, 9, 3, 335, 42, 4, 0, 555, 888};
+    int heap_size = vec.size();  
 
-vector <int> vec = {0,1,9,3,35,42,4,0,55,8};
-MakeHeap(vec.size(),vec);
+    MakeHeap(vec, heap_size);
 
-for (int i : vec)
-{
-cout << " " <<i;
+    for (int i : vec)
+    {
+        cout << " " << i;
+    }
 
-}
-
+    return 0;
 }
