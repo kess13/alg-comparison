c78b30012ddb21df4092d42150046e358d63c05e
kess13 <bohhhdan13@gmail.com>
2023-10-17 19:17:39 +0200

    started to make new logic

--- heap.cc
+++ heap.cc
@@ -7,9 +7,22 @@ using namespace std;
 
 
 void heapify(vector<int> &vec, int i, int size) {
-    int CurrentEl = i;
+    int CurrentElIndex = i;
     int LeftChild = 2 * i + 1;
     int RightChild = 2 * i + 2;
+      if ((vec[LeftChild]>vec[CurrentElIndex] && LeftChild<size) || (vec[RightChild]>vec[CurrentElIndex] && RightChild<size))
+      {
+        if (vec[LeftChild]>vec[RightChild] && vec[LeftChild]>vec[CurrentElIndex])
+        swap(vec[LeftChild],vec[CurrentElIndex]);
+         if (vec[LeftChild]<vec[RightChild] && vec[RightChild]>vec[CurrentElIndex])
+        swap(vec[RightChild],vec[CurrentElIndex]);
+
+
+
+
+
+      }
+
     
 }
 
