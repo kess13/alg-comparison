3c49417623b3d604e51ad0a0f30d7da2966e5618
kess13 <bohhhdan13@gmail.com>
2023-10-17 19:00:07 +0200

    heapify func is uptadet a little bit cause it didnt see the last element

--- heap.cc
+++ heap.cc
@@ -10,7 +10,7 @@ void heapify(vector<int> &vec, int i, int size) {
     int CurrentEl = i;
     int LeftChild = 2 * i + 1;
     int RightChild = 2 * i + 2;
-    if (LeftChild < size && RightChild < size) {
+    if (LeftChild <=size && RightChild <= size) {
         while (max(vec[LeftChild], vec[RightChild]) > vec[CurrentEl]) {
             if (vec[LeftChild] > vec[RightChild]) {
                 if (vec[LeftChild] > vec[CurrentEl]) {
@@ -32,8 +32,8 @@ void MakeHeap(vector<int> &vec, int heap_size) {
         heapify(vec, i, heap_size);
     }
     
+
     
-    swap(vec[0], vec[heap_size - 1]);
 
 }
 
@@ -47,7 +47,7 @@ void MakeHeap(vector<int> &vec, int heap_size) {
 
 int main()
 {
-    vector<int> vec = {0, 1, 9, 3, 335, 42, 4, 0, 555, 888};
+    vector<int> vec = {0, 1, 9, 83, 335, 42, 4, 0, 5545, 8888};
     int heap_size = vec.size();  
 
     MakeHeap(vec, heap_size);
--- radix.cc
+++ radix.cc
@@ -53,7 +53,7 @@ vector<int> radix(vector<int> vec) {
 int main()
 {
 
-vector <int> vec = {0,1,9,3,35,42,4,0,55,8};
+vector <int> vec = {0, 1, 9, 3, 3335, 42, 4, 0, 5545, 888};
 
 vec=radix(vec);
 for (int i : vec)
