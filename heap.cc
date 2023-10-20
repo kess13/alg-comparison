28c52ee9c88021d601bdcae7efcdc6cfa2aad175
kess13 <bohhhdan13@gmail.com>
2023-10-20 20:42:47 +0200

    still have logical issues

--- heap.cc
+++ heap.cc
@@ -4,7 +4,7 @@ using namespace std;
 #include <algorithm>
 
 
-
+bool swapp=false;
 
 void heapify(vector<int> &vec, int i, int size) {
     int CurrentElIndex = i;
@@ -12,18 +12,24 @@ void heapify(vector<int> &vec, int i, int size) {
     int RightChild = 2 * i + 2;
     
       
-        if (LeftChild < size && vec[LeftChild] > vec[CurrentElIndex])
+       if (LeftChild < size && vec[LeftChild] > vec[CurrentElIndex])
        { swap(vec[CurrentElIndex], vec[LeftChild]); 
-       heapify(vec, CurrentElIndex , size);
+       swapp=true;
        }
 
    
   if (RightChild < size && vec[RightChild] > vec[CurrentElIndex])
-       { swap(vec[CurrentElIndex], vec[RightChild]);
-heapify(vec, CurrentElIndex , size);
+       { swap(vec[CurrentElIndex], vec[RightChild]); swapp=true;
        }
+     if (swapp)
+     {
+//to restore heap property
+  swapp=false;
+     heapify(vec,CurrentElIndex, size);
 
-      
+     
+   
+     }
 }
 
 
@@ -31,13 +37,16 @@ heapify(vec, CurrentElIndex , size);
 void MakeHeap(vector<int> &vec, int heap_size) {
     for (int i = heap_size / 2 - 1; i >= 0; i--) {
         heapify(vec, i, heap_size);
+    
     }
 }
 
 void MainSort(vector<int> &vec, int heap_size) {
     MakeHeap(vec, heap_size);
     for (int i = heap_size - 1; i >= 0; i--) {
-        heapify(vec, i , heap_size-i);
+           swap(vec[0], vec[i]);
+        heapify(vec, 0, i);
+     
 
     }
 }
