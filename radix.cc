857c5af779011ae1a216e205e6ae2f6b332b3f84
kess13 <bohhhdan13@gmail.com>
2023-10-21 23:37:06 +0200

    analyzing code and trying to find logical problem

--- radix.cc
+++ radix.cc
@@ -6,18 +6,19 @@ using namespace std;
 vector <int> countingsort(vector<int> vec)
 {
 
-auto max = max_element(vec.begin(), vec.end());
+auto max = max_element(vec.begin(), vec.end());//Find out the maximum element from the given array.
 int maxel= *max;
 int temp=0;
-vector<int> countarr(maxel+1,0);
+vector<int> countarr(maxel+1,0);//Initialize a countArray[] of length max+1 with all elements as 0. This array will be used for storing the occurrences of the elements of the input array.
+
 for (int i=0; i <= maxel; i++)
 {
-countarr[i]=count(vec.begin(), vec.end(), i);
+countarr[i]=count(vec.begin(), vec.end(), i);//Right.In the countArray[], store the count of each unique element of the input array at their respective indices.
 }
-for (int i=0; i <= maxel; i++) 
+for (int i=1; i <= maxel; i++) 
 {
-temp=temp+countarr[i];
-countarr[i]=temp;
+                      //Store the cumulative sum or prefix sum of the elements of the countArray[] . right
+countarr[i] = countarr[i - 1] + countarr[i];
 }
 vector<int> res(vec.size());
  for (int i = vec.size() - 1; i >= 0; i--) {
@@ -53,7 +54,7 @@ vector<int> radix(vector<int> vec) {
 int main()
 {
 
-vector <int> vec = {1231230, 1, 9, 3,3,3, 3335, 42, 4, 0, 5545, 88328};
+vector <int> vec = {1231230, 1, 9, 3,3,3, 3335, 42, 4, 0, 5545,3, 88328};
 
 vec=radix(vec);
 for (int i : vec)
