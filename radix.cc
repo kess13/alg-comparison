73f2ae0a68eaedd49123deeb8538ce475b5795d4
kess13 <bohhhdan13@gmail.com>
2023-10-28 16:26:07 +0200

    final radix with test

--- radix.cc
+++ radix.cc
@@ -3,6 +3,49 @@
 using namespace std;
 #include <algorithm>
 #include <string>
+#include <chrono>
+#include <thread> 
+#include <cstdlib>
+
+vector<int> generateRandomArray(int size) {
+    vector<int> vec(size);
+    for (int i = 0; i < size; i++) {
+        vec[i] = rand(); 
+    }
+    return vec;
+}
+vector<int> SortedArr(int size) {
+    vector<int> vec(size);
+    for (int i = 0; i < size; i++) {
+        vec[i] = i; 
+    }
+    return vec;
+}
+vector<int> ReversedSortedArr(int size) {
+    vector<int> vec(size);
+    for (int i = size-1;  i>=0 ; i--) {
+        vec[i] = i; 
+    }
+    return vec;
+}
+
+struct Timer
+{
+std::chrono::high_resolution_clock::time_point start, end;
+chrono::duration<float> duration;
+Timer()
+{
+start =chrono::high_resolution_clock::now();
+}
+
+~Timer()
+{
+end = chrono::high_resolution_clock::now();
+duration = end - start;
+float ms = duration.count() * 1000.0f;
+cout<<"Timer took "<< ms <<"ms"<<endl;
+}
+};
 void countingsort(vector<int> vec, vector<int> &MainVec) {
     if (vec.empty()) return;
     auto max = max_element(vec.begin(), vec.end());
@@ -26,6 +69,7 @@ countarr[i]=count(vec.begin(),  vec.end(), i);
     
 }
 void radix(vector<int> &vec) {
+    Timer timer;
      if (vec.empty()) return ;
     auto max = max_element(vec.begin(), vec.end());
     int maxel = *max;
@@ -53,59 +97,8 @@ int multiplier=1;
 
 int main()
 {
-vector<int> testVector1 = {3, 2, 1};
-vector<int> testVector2 = {5, 7, 2, 9, 1, 3};
-vector<int> testVector3 = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-vector<int> testVector4 = {1, 1, 1, 1, 1, 1};
-vector<int> testVector5 = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-vector<int> testVector6 = {100, 90, 80, 70, 60, 50, 40, 30, 20, 10};
-vector<int> testVector7 = {2};
-vector<int> testVector8 = {}; 
-vector <int> testVector9 = {10234, 15467, 19876, 14567, 17890, 12345, 16789, 18765, 19876, 15678}; 
- radix(testVector1);
-  radix(testVector2);
-   radix(testVector3);
-   radix(testVector4);
-    radix(testVector5);
-     radix(testVector6);
-       radix(testVector7);
-       radix(testVector8);
- radix(testVector9);
-if (is_sorted(testVector1.begin(),testVector1.end()))
-{
-cout<<"success! Vector 1 passed"<<endl;
-}
-if ( is_sorted(testVector2.begin(),testVector2.end()))
-{
-cout<<"success! Vector 2 passed"<<endl;
-}
-if (is_sorted(testVector3.begin(),testVector3.end()))
-{
-cout<<"success! Vector 3 passed"<<endl;
-}
-if (is_sorted(testVector4.begin(),testVector4.end()))
-{
-cout<<"success! Vector 4 passed"<<endl;
-}
-if (is_sorted(testVector5.begin(),testVector5.end()))
-{
-cout<<"success! Vector 5 passed"<<endl;
-}
-if (is_sorted(testVector6.begin(),testVector6.end()))
-{
-cout<<"success! Vector 6 passed"<<endl;
-}
-if (is_sorted(testVector7.begin(),testVector7.end()))
-{
-cout<<"success! Vector 7 passed"<<endl;
-}
-if ( is_sorted(testVector8.begin(),testVector8.end()))
-{
-cout<<"success! Vector 8 passed"<<endl;
-}
-if ( is_sorted(testVector9.begin(),testVector9.end()))
-{
-cout<<"success! Vector 9 passed"<<endl;
-}
+vector <int> large= generateRandomArray(10000);
+radix(large);
+  
 return 0;
 }
