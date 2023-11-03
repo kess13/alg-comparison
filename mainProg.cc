f36cd335e41b9cb220188c4935b60f57085c659c
kess13 <bohhhdan13@gmail.com>
2023-11-03 16:19:16 +0100

    Update the average execution time count and verification for correctness for sorts

--- mainProg.cc
+++ mainProg.cc
@@ -11,6 +11,17 @@ using namespace std;
 int main()
 {
     Timer timer;
-vector <int> vec = generateRandomArray(100);
-radix(vec);
+    int sortedRightTimes = 0;
+    int AmountTests = 10;
+    for (int i = 0; i < AmountTests; i++)
+    {
+        vector<int> vec = generateRandomArray(10000);
+        MainSort(vec, vec.size());
+        if (is_sorted(vec.begin(), vec.end()))
+            sortedRightTimes++;
+        timer.stop(); 
+    }
+    if (AmountTests==sortedRightTimes)
+    cout <<"All passed" << endl;
+    cout << "Average Execution Time: " << timer.totalTime/ AmountTests  << "ms" << endl;
 }
