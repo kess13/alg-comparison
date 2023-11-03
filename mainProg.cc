a36c00bed4e3dcdd0f184ac4dcce58bcc00d97a5
kess13 <bohhhdan13@gmail.com>
2023-11-03 22:44:19 +0100

    updated main prog for better tests and execution time comparison

--- mainProg.cc
+++ mainProg.cc
@@ -12,16 +12,20 @@ int main()
 {
     Timer timer;
     int sortedRightTimes = 0;
-    int AmountTests = 10;
-    for (int i = 0; i < AmountTests; i++)
+    const int AmountOfTests = 100;
+    vector<int> testSizes = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
+    for (int sizes : testSizes){
+    for (int i = 0; i < AmountOfTests; i++)
     {
-        vector<int> vec = generateRandomArray(10000);
+        vector<int> vec = SortedArr(sizes);
         MainSort(vec, vec.size());
         if (is_sorted(vec.begin(), vec.end()))
             sortedRightTimes++;
-        timer.stop(); 
+        timer.stop();
     }
-    if (AmountTests==sortedRightTimes)
-    cout <<"All passed" << endl;
-    cout << "Average Execution Time: " << timer.totalTime/ AmountTests  << "ms" << endl;
+    cout << "Average Execution Time for size "<<sizes << timer.totalTime / AmountOfTests << endl;
+    }
+    if (AmountOfTests*testSizes.size() == sortedRightTimes)
+        cout << "All passed" << endl;
+    
 }
