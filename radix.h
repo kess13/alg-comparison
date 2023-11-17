3340b3868e62874510fa7dce3bc59c5c85ecb00b
kess13 <bohhhdan13@gmail.com>
2023-11-17 19:06:09 +0100

    small mistake

--- radix.h
+++ radix.h
@@ -20,7 +20,7 @@ void countingsort(vector<int> &vec, int multiplier)
 
     }
    
-    for (int i = 1; i <= 10; i++)
+    for (int i = 1; i < 10; i++)
     {
         countarr[i] += countarr[i - 1];
     }
